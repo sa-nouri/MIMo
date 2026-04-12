666477127dd203c9d14c71830cce2effc849f7be
Salar Nouri <salr.nouri@gmail.com>
2026-04-12 15:18:38 +0330

    Fix: model and data initialization in simuation (MIMoEnv)

--- mimoEnv/envs/mimo_env.py
+++ mimoEnv/envs/mimo_env.py
@@ -378,7 +378,8 @@ class MIMoEnv(MujocoEnv, utils.EzPickle):
         self._set_observation_space()
 
     def _initialize_simulation(self,):
-        super()._initialize_simulation()
+        model, data = super()._initialize_simulation()
+        self.model, self.data = model, data
 
         fps = int(np.round(1 / self.dt))
         self.metadata = {
@@ -399,6 +400,8 @@ class MIMoEnv(MujocoEnv, utils.EzPickle):
         self._set_initial_position(self._initial_qpos)
 
         self.actuation_model = self.actuation_model(self, self.mimo_actuators)
+
+        return model, data
 
     @property
     def n_actuators(self):
